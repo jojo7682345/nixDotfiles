570058675454457cb873c31ac51c50e15fe9d663
dms <jojo7682345@gmail.com>
2025-06-08 22:08:01 +0200

    progress

--- settings/system/users.nix
+++ settings/system/users.nix
@@ -1,13 +1,60 @@
-{ inputs, pkgs, machine, lib, ... }:
+{ 
+	lib,
+	self,
+	self',
+	config,
+	inputs,
+	inputs',
+	machine,
+	systemStats,	
+	... 
+}:
+let 
+	inherit (lib) genAttrs;
+in
 {
 
+	users.users = builtins.listToAttrs (map (user: {
+		name = user.name;
+		value = {
+			isNormalUser = true;
+			extraGroups = [] ++ 
+				(lib.optionals (
+					user.hasNetworkAccess && machine.os.network.enableTui  
+				) "networkmanager") ++
+				(lib.optionals (
+					user.isAdmin
+				) "wheel");
+		};
+	}) machine.users);
+	home-manager = {
+		verbose = true;
+		useUserPackages = true;
+		useGlobalPkgs = true;
+		backupFileExtension = "bak";
+		
+		users = builtins.listToAttrs (map (user: {
+			name = user.name;
+			value = {
+				imports = (lib.optionals
+					(builtins.pathExists "${self}/home/${user.name}") 
+					[ "${self}/home/${user.name}" ]
+				);
+				home.stateVersion = "25.05";
+				home.username = user.name;
+				home.homeDirectory = "/home/${user.name}";
+			};
+		}) machine.users);
 
-	users.users.dms = {
-		isNormalUser = true;
-		description = "dms";
-		extraGroups = [ "networkmanager" "wheel"];
-		packages = with pkgs; [];
+		extraSpecialArgs = {
+			inherit
+				self
+				self'
+				inputs
+				inputs'
+				machine
+				systemStats
+			;
+		};
 	};
-
-
 }
