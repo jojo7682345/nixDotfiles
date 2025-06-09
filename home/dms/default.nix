4c92d36ca8f120239797b982aa07f1c241444d25
jojo7682345 <jojo7682345@gmail.com>
2025-06-09 14:37:47 +0200

    Progress

--- home/dms/default.nix
+++ home/dms/default.nix
@@ -1,11 +1,18 @@
 { inputs, machine, config, pkgs,  ... }:
 {
 	home.packages = with pkgs; [ git zsh ];
-	programs.zsh.enable = true;
-
-	programs.git = {
-		enable = true;
-		userEmail = "jojo7682345@gmail.com";
-		userName = "jojo7682345";
+	programs = {
+		zsh = {
+			enable = true;
+			ohMyZsh = {
+				enable = true;
+				plugins = [ "git" "sudo" ];
+			};
+		};
+		git = {
+			enable = true;
+			userEmail = "jojo7682345@gmail.com";
+			userName = "jojo7682345";
+		};
 	};
 }
